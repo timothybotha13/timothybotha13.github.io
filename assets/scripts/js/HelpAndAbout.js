aee5eabbb43c4632abbee1283860580564031fa2
timothybotha13 <143791506+timothybotha13@users.noreply.github.com>
2023-09-01 09:30:53 +0100

    Update HelpAndAbout.js

--- assets/scripts/js/HelpAndAbout.js
+++ assets/scripts/js/HelpAndAbout.js
@@ -10,10 +10,10 @@ function GetHelpAndAboutContent() {
             <p><span>Project:</span> Community helper.</p>
             <p><span>Version:</span> ${GetVersion()}.</p>
             <p><span>Author:</span> Callum Martin.</p>
-            <p><span>Admins:</span> Callum Martin.</p>
+            <p><span>Admins:</span> Timothy Botha.</p>
             <p><span>Description:</span> Aligns the input and output of MDS.</p>
-            <p><span>Github:</span> <a href="https://github.com/CallumMartinA/CallumMartinA.github.io">Github.</a></p>
-            <p><span>Report an issue:</span> <a href="https://github.com/CallumMartinA/CallumMartinA.github.io/issues">Report an issue</a>, or contact an admin via email.</p>
+            <p><span>Github:</span> <a href="https://github.com/timothybotha13/timothybotha13.github.io">Github.</a></p>
+            <p><span>Report an issue:</span> <a href="https://github.com/timothybotha13/timothybotha13.github.io/issues">Report an issue</a>, or contact an admin via email.</p>
         </div>
     `;
 
